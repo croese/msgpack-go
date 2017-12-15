2370ecbc33a29b66b4d59ea8b98a8cf64e847cdc
croese <croese@gmail.com>
2017-12-15 11:54:29 -0600

    Adds uint support and tests

--- msgpack.go
+++ msgpack.go
@@ -2,9 +2,11 @@ package msgpack
 
 import (
 	"bytes"
-	"fmt"
+	"math"
 	"reflect"
 )
+
+const maxPositiveFixnum = 0x7f
 
 func Marshal(i interface{}) ([]byte, error) {
 	var buf bytes.Buffer
@@ -22,9 +24,55 @@ func Marshal(i interface{}) ([]byte, error) {
 			}
 		case reflect.Int, reflect.Int8, reflect.Int16,
 			reflect.Int32, reflect.Int64:
-			fmt.Printf("DEBUG: %d\n", v.Int())
+			ival := v.Int()
+			if isFixnum(ival) {
+				buf.WriteByte(byte(ival))
+			} else if ival >= 0 {
+				WriteUint(uint64(ival), &buf)
+			} else {
+				buf.WriteByte(0xd0)
+				buf.WriteByte(byte(ival))
+			}
+		case reflect.Uint,
+			reflect.Uint8, reflect.Uint16, reflect.Uint32,
+			reflect.Uint64:
+			WriteUint(v.Uint(), &buf)
 		}
 	}
 
 	return buf.Bytes(), nil
+}
+
+func WriteUint(uval uint64, buf *bytes.Buffer) {
+	if uval <= maxPositiveFixnum {
+		buf.WriteByte(byte(uval))
+	} else if uval <= math.MaxUint8 {
+		buf.WriteByte(0xcc)
+		buf.WriteByte(byte(uval))
+	} else if uval <= math.MaxUint16 {
+		u16 := uint16(uval)
+		buf.WriteByte(0xcd)
+		buf.WriteByte(byte(u16 >> 8))
+		buf.WriteByte(byte(u16 & 0x00FF))
+	} else if uval <= math.MaxUint32 {
+		u32 := uint32(uval)
+		buf.WriteByte(0xce)
+		buf.WriteByte(byte(u32 >> 24))
+		buf.WriteByte(byte((u32 & 0x00FF0000) >> 16))
+		buf.WriteByte(byte((u32 & 0x0000FF00) >> 8))
+		buf.WriteByte(byte((u32 & 0x000000FF)))
+	}
+	buf.WriteByte(0xcf)
+	buf.WriteByte(byte(uval >> 56))
+	buf.WriteByte(byte((uval & 0x00FF000000000000) >> 48))
+	buf.WriteByte(byte((uval & 0x0000FF0000000000) >> 40))
+	buf.WriteByte(byte((uval & 0x000000FF00000000) >> 32))
+	buf.WriteByte(byte((uval & 0x00000000FF000000) >> 24))
+	buf.WriteByte(byte((uval & 0x0000000000FF0000) >> 16))
+	buf.WriteByte(byte((uval & 0x000000000000FF00) >> 8))
+	buf.WriteByte(byte((uval & 0x00000000000000FF)))
+}
+
+func isFixnum(n int64) bool {
+	return (n >= 0 && n <= maxPositiveFixnum) || (n < 0 && n >= -32)
 }
--- msgpack_test.go
+++ msgpack_test.go
@@ -1,6 +1,7 @@
 package msgpack
 
 import (
+	"math"
 	"testing"
 )
 
@@ -13,30 +14,52 @@ func TestMarshal(t *testing.T) {
 		{input: true, expected: []byte{0xc3}},
 		{input: false, expected: []byte{0xc2}},
 		{input: 0, expected: []byte{0}},
+		{input: uint8(4), expected: []byte{0x04}},
+		{input: 127, expected: []byte{0x7f}},
+		{input: 128, expected: []byte{0xcc, 0x80}},
+		{input: uint8(129), expected: []byte{0xcc, 0x81}},
+		{input: 255, expected: []byte{0xcc, 0xff}},
+		{input: 256, expected: []byte{0xcd, 0x01, 0x00}},
+		{input: uint16(257), expected: []byte{0xcd, 0x01, 0x01}},
+		{input: 65535, expected: []byte{0xcd, 0xff, 0xff}},
+		{input: uint16(65535), expected: []byte{0xcd, 0xff, 0xff}},
+		{input: 65536, expected: []byte{0xce, 0x00, 0x01, 0x00, 0x00}},
+		{input: uint32(65537), expected: []byte{0xce, 0x00, 0x01, 0x00, 0x01}},
+		{input: 4294967295, expected: []byte{0xce, 0xff, 0xff, 0xff, 0xff}},
+		{input: uint32(4294967295), expected: []byte{0xce, 0xff, 0xff, 0xff, 0xff}},
+		{input: 4294967296, expected: []byte{0xcf, 0x00, 0x00, 0x00, 0x01,
+			0x00, 0x00, 0x00, 0x00}},
+		{input: uint64(4294967297), expected: []byte{0xcf, 0x00, 0x00, 0x00, 0x01,
+			0x00, 0x00, 0x00, 0x01}},
+		{input: uint64(math.MaxUint64), expected: []byte{0xcf, 0xff, 0xff, 0xff, 0xff,
+			0xff, 0xff, 0xff, 0xff}},
+		{input: -1, expected: []byte{0xff}},
+		{input: -32, expected: []byte{0xe0}},
+		{input: -33, expected: []byte{0xd0, 0xdf}},
 	}
 
-	for i, test := range tests {
+	for _, test := range tests {
 		b, e := Marshal(test.input)
-		checkMarshalReturns(i, b, e, t)
-		compareByteSlices(i, test.expected, b, t)
+		checkMarshalReturns(test.input, b, e, t)
+		compareByteSlices(test.input, test.expected, b, t)
 	}
 }
 
-func checkMarshalReturns(testNumber int, b []byte, e error, t *testing.T) {
+func checkMarshalReturns(input interface{}, b []byte, e error, t *testing.T) {
 	if e != nil {
-		t.Fatalf("test %d: error should be nil", testNumber)
+		t.Fatalf("input %v: error should be nil", input)
 	}
 
 	if b == nil {
-		t.Fatalf("test %d: returned bytes should not be nil", testNumber)
+		t.Fatalf("input %v: returned bytes should not be nil", input)
 	}
 }
 
-func compareByteSlices(testNumber int, left []byte, right []byte, t *testing.T) {
-	for i, b := range left {
-		if right[i] != b {
-			t.Errorf("test %d: mismatched byte at index %d. left=%x, right=%x",
-				testNumber, i, b, right[i])
+func compareByteSlices(input interface{}, expected []byte, actual []byte, t *testing.T) {
+	for i, b := range expected {
+		if actual[i] != b {
+			t.Errorf("input %v: mismatched byte at index %d. expected=%x, actual=%x",
+				input, i, b, actual[i])
 		}
 	}
 }
